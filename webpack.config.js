0ff7af6c26b127a6e4e5e821981aab5037becb06
Tyler Carver <carver230620@cua.edu>
2019-09-07 15:04:22 -0600

    Added html webpack plugin and config for creating a hash for the client app.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
 
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
 	target: 'node',
 	entry: './client/app.js',
 	output: {
-		path: __dirname + '/assets/js',
-		filename: 'app.js',
+		path: __dirname + '/assets/js/client',
+		publicPath: '/assets/js/client',
+		filename: 'app-[hash].js',
 	},
 	devtool: 'source-map',
 	module: {
 		rules: [
-		  { test: /\.hbs$/, loader: "handlebars-loader" }
+			{
+				test: /\.hbs$/,
+				loader: "handlebars-loader",
+				include: /client/,
+			}
 		]
-	  },
+	},
+	plugins: [new HtmlWebpackPlugin(
+		{
+			filename: '../../../server/app/layouts/main.layout.hbs',
+			template: './server/app/layouts/src/main.layout.hbs',
+			inject: false,
+		}
+	)],
 }
