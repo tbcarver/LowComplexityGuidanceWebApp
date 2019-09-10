a7a1a11476c4f1bf32fea155e515bdfefd71b7b3
Tyler Carver <carver230620@cua.edu>
2019-09-10 09:59:04 -0600

    Added webpack building of sass and configuring the hbs layout file with a hash.

--- client/app.js
+++ client/app.js
@@ -2,6 +2,10 @@
 // NOTE: This file is for client features that should be available on every server side served webpage.
 //  Features that are for a single page should have there own js entry point file and webpack configuration.
 
+// Import all sass sources here for use with webpack
+// These are output into a assets/dist/main.css file with a cache busting hash
+import style from './sass/main.scss';
+
 // Import in all client web components
 import { MessageBox } from "./webComponents/messageBoxes/messageBox.component";
 import { MessageBoxClosable } from "./webComponents/messageBoxes/messageBoxClosable.component";
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,52 @@
 
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
 	target: "web",
 	mode: "development",
 	entry: "./client/app.js",
 	output: {
-		path: __dirname + "/assets/js/dist",
-		publicPath: "/assets/js/dist",
+		path: __dirname + "/assets/dist",
+		publicPath: "/assets/dist",
 		filename: "app-[contenthash].js",
 	},
 	devtool: "source-map",
 	module: {
 		rules: [
 			{
+				test: /\.scss$/,
+				use: [
+					"style-loader",
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					{
+						loader: "sass-loader",
+						options: {
+							sassOptions: {
+								includePaths: ['node_modules/bootstrap/scss'],
+							},
+						},
+					},
+				]
+			},
+			{
 				test: /\.hbs$/,
 				loader: "handlebars-loader",
 				include: /client/,
-			}
+			},
 		]
 	},
-	plugins: [new HtmlWebpackPlugin(
-		{
-			filename: "../../../server/app/layouts/dist/main.layout.hbs",
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "main-[contenthash].css",
+		}),
+		new HtmlWebpackPlugin({
+			filename: "../../server/app/layouts/dist/main.layout.hbs",
 			template: "./server/app/layouts/src/main.layout.hbs",
 			inject: false,
-		}
-	)],
+		}),
+	],
 }
