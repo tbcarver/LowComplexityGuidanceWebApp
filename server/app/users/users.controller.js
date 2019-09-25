11aee5063d61229c1d9654565a57b550620ce9fe
Tyler Carver <carver230620@cua.edu>
2019-09-25 08:57:11 -0600

    Users clean up.

--- server/app/users/users.controller.js
+++ server/app/users/users.controller.js
@@ -3,69 +3,59 @@
 var usersStore = require("../../store/usersStore")
 var _ = require("lodash");
 
-
 function initialize(app, acl) {
 
     // acl noop
     app.get("/users", getUsers);
-    app.get("/user/new", getUserNew);
-    app.post("/users", addUser);
-    app.get("/user/edit/:userId", editUser);
-    // app.post("/user/edit", postUserEdit);
 
+    // acl noop
+    app.get("/user/new", getNew);
+    app.get("/user/edit/:userId", getEdit);
+    app.post("/user/edit", postEdit);
 }
 
-/** @param { Request } req @param { Response } res */
-function getUserNew(req, res) {
-
-    var model = new AppModel(req, "New User");
-    model.layout = "oneColumn.layout.hbs";
-    // var model = new AppModel(req, "New Article", "New Article");
-    res.render("users/usersDetails.template.hbs", model);
-};
-
-/** @param { Request } req @param { Response } res */
-function addUser(req, res) {
-    // if(!req.body) return //error
-    var model = new AppModel(req, "New User");
-    model.layout = "oneColumn.layout.hbs";
-
-    var user = _.clone(req.body);
-    user.roles = [];
-
-    if (req.body.roles) {
-
-        if (typeof req.body.roles === "string") {
-
-            user.roles.push(req.body.roles)
-        } else {
-            user.roles = req.body.roles;
-        }
-    }
-    model.users = usersStore.addUser(user);
-
-    res.render("users/usersDetails.template.hbs", model);
-};
-
-/** @param { Request } req @param { Response } res */
 function getUsers(req, res) {
 
     var model = new AppModel(req, "Users");
     model.layout = "oneColumn.layout.hbs";
-
     model.users = usersStore.getUsers();
 
     res.render("users/usersMaster.template.hbs", model);
 };
 
-/** @param { Request } req @param { Response } res */
-function editUser(req, res) {
+function getNew(req, res) {
+
+    var model = new AppModel(req, "New User");
+    model.layout = "oneColumn.layout.hbs";
+    
+    res.render("users/usersDetailsEdit.template.hbs", model);
+};
+
+function getEdit(req, res) {
+
     var model = new AppModel(req, "Edit");
     model.layout = "oneColumn.layout.hbs";
-
     model.user = usersStore.getUser(req.params.userId);
-    res.render("users/usersDetails.template.hbs", model)
 
+    res.render("users/usersDetailsEdit.template.hbs", model)
 }
+
+function postEdit(req, res) {
+
+    var user = _.clone(req.body);
+    user.roles = [];
+
+    if (req.body.roles) {
+        if (typeof req.body.roles === "string") {
+            user.roles.push(req.body.roles)
+        } else {
+            user.roles = req.body.roles;
+        }
+    }
+
+    req.params.userId = usersStore.addUser(user);
+    getEdit(req, res);
+};
+
 
 module.exports.initialize = initialize;
