195f66f1bd22745aea9ed8239ea456a15b4dda79
Tyler Carver <carver230620@cua.edu>
2019-10-07 15:18:30 -0600

    Added unique constraint to UsersRoles.

--- sql/createDatabase/tables/usersRoles.sql
+++ sql/createDatabase/tables/usersRoles.sql
@@ -2,7 +2,8 @@
 CREATE TABLE UsersRoles (
     usersRolesId INTEGER PRIMARY KEY AUTOINCREMENT,
     userId INTEGER NOT NULL REFERENCES Users (userId),
-    roleID INTEGER NOT NULL REFERENCES Roles (roleId)
+    roleID INTEGER NOT NULL REFERENCES Roles (roleId),
+    UNIQUE (userId, roleID)
 );
 
 INSERT INTO UsersRoles (userId, roleID) VALUES (1, 1);
