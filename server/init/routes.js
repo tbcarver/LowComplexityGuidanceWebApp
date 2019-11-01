c82cd6ea1953c4ec2deeadd5f796e473a841bc0d
Tyler Carver <carver230620@cua.edu>
2019-11-01 07:06:15 -0600

    Init routes formatting.

--- server/init/routes.js
+++ server/init/routes.js
@@ -10,21 +10,17 @@ function initialize(app, acl) {
 	app.use(favicon(path.join(__dirname, "../../assets/images", "favicon.ico")));
 
 	var oneYearInMilliseconds = 60 * 1000 * 60 * 24 * 365; // 31536000
+	var staticOptions = { maxAge: oneYearInMilliseconds };
 
 	acl.allow("public starts with", "/assets", "*");
-	app.use("/assets", express.static("./assets", { maxAge: oneYearInMilliseconds }));
-	app.use("/assets/vendor/bootstrap/4.3.1/", express.static("./node_modules/bootstrap/dist",
-		{ maxAge: oneYearInMilliseconds }));
+	app.use("/assets", express.static("./assets", staticOptions));
+	app.use("/assets/vendor/bootstrap/4.3.1/", express.static("./node_modules/bootstrap/dist", staticOptions));
 	app.use("/assets/vendor/fontawesome-free/5.9.0/", express.static("./node_modules/@fortawesome/fontawesome-free",
-		{ maxAge: oneYearInMilliseconds }));
-	app.use("/assets/vendor/fontawesome/4.7.0/", express.static("./node_modules/font-awesome",
-		{ maxAge: oneYearInMilliseconds }));
-	app.use("/assets/vendor/jquery/3.3.1/", express.static("./node_modules/jquery/dist",
-		{ maxAge: oneYearInMilliseconds }));
-	app.use("/assets/vendor/typeahead.js/0.11.1/", express.static("./node_modules/typeahead.js/dist",
-		{ maxAge: oneYearInMilliseconds }));
-	app.use("/assets/vendor/bootstrap-notify/3.1.3/", express.static("./node_modules/bootstrap-notify",
-		{ maxAge: oneYearInMilliseconds }));
+		staticOptions));
+	app.use("/assets/vendor/fontawesome/4.7.0/", express.static("./node_modules/font-awesome", staticOptions));
+	app.use("/assets/vendor/jquery/3.3.1/", express.static("./node_modules/jquery/dist", staticOptions));
+	app.use("/assets/vendor/typeahead.js/0.11.1/", express.static("./node_modules/typeahead.js/dist", staticOptions));
+	app.use("/assets/vendor/bootstrap-notify/3.1.3/", express.static("./node_modules/bootstrap-notify", staticOptions));
 
 	directoryWalkerSync.walkDirectory("./server/api", null, null, function(filePathName, stats) {
 
